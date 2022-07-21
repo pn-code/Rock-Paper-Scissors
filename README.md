2be370296dd20d5b28638a4f6a1e010410df7cb3
Philip Nguyen <philipnguyen953@gmail.com>
2022-07-21 11:28:42 -0700

    V1.0 Rock, Paper, Scissors

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Rock-Paper-Scissors
+
+My first JavaScript project showcasing Rock, Paper, Scissors.
+
+This project shows the fundamentals of Javascript, including different data
+types, functions, looping, and conditionals.
---
+++ javascript.js
@@ -0,0 +1,89 @@
+//functions
+let playerScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    let random = Math.floor(Math.random() * 10);
+    if (random <= 3) {
+        let computerChoice = "rock";
+        return computerChoice;
+    } else if (random >= 7) {
+        let computerChoice = "paper";
+        return computerChoice;
+    } else {
+        let computerChoice = "scissors";
+        return computerChoice;
+    }
+};
+
+let computerChoice;
+
+
+function roundResults (playerChoice, computerChoice) {
+
+    if (computerChoice == playerChoice) {
+        let result = "It's a draw";
+        return result;
+    
+    } else if (playerChoice == "rock" && computerChoice == "scissors" ||
+               playerChoice == "scissors" && computerChoice == "paper" ||
+               playerChoice == "paper" && computerChoice == "rock") {
+    
+                    let result = "You win!";
+                    playerScore = ++playerScore;
+                    computerScore = --computerScore;
+                    return result;
+    
+    } else if (playerChoice == "rock" && computerChoice == "paper" ||
+               playerChoice == "scissors" && computerChoice == "rock" ||
+               playerChoice == "paper" && computerChoice == "scissors") {
+                
+                    let result = "YOU LOSE, AHAHAHAHA. IDIOT!";
+                    playerScore = --playerScore;
+                    computerScore = ++computerScore;
+                    return result;
+    
+    }else {
+        let result = "You've inputted the wrong key; this will count against you!" ; 
+        playerScore = --playerScore;
+        computerScore = ++computerScore;
+        return result;
+
+    }
+    };
+    
+
+function playRound() {
+
+    getComputerChoice();
+    computerChoice = getComputerChoice();
+    
+    let playerChoice = prompt("Rock, paper, or scissors");
+    
+    console.log("You chose " +playerChoice);
+    console.log("The computer chose " +computerChoice);
+    
+    
+    let result = roundResults(playerChoice, computerChoice);
+    console.log(result);
+    }
+
+//rock paper scissors
+
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+}
+
+game();
+
+if (playerScore > 0) {
+    console.log("Congratulations, you won by " + playerScore)
+}else if (playerScore == computerScore){
+    console.log("There has been a draw.")
+}else{
+    console.log("Unfortunately, you have lost by " + computerScore)
+}
+
